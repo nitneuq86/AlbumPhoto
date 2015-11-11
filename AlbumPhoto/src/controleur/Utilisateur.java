abe85b7214c77b4595b2a2794c81ad4b6e2ac7cd
Myriam <myriam.abdelfattah@gmail.com>
2015-11-11 18:51:59 +0100

    ajout controleurs

---
+++ AlbumPhoto/src/controleur/Utilisateur.java
@@ -0,0 +1,41 @@
+package controleur;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import extra.Data;
+
+@WebServlet(urlPatterns={"/Utilisateur","/Utilisateur/*"})
+public class Utilisateur extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+    public Utilisateur() {
+        super();
+    }
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String path = request.getPathInfo();
+		if(path==null || path.compareTo("/")==0) {
+			request.setAttribute("typeRecherche", "utilisateur");
+			this.getServletContext().getRequestDispatcher("/rechercher.jsp").forward(request, response);
+		}
+		else {
+			try {
+				int idRessource = Integer.parseInt(path.substring(1));
+				if(idRessource == 245) {
+					this.getServletContext().getRequestDispatcher("/utilisateur.jsp").forward(request, response);
+				}
+				else this.getServletContext().getRequestDispatcher("/ressourceIntrouvable.jsp").forward(request, response);
+			}
+			catch(NumberFormatException ex) {
+				this.getServletContext().getRequestDispatcher("/ressourceIntrouvable.jsp").forward(request, response);
+			}
+		}
+	}
+
+}
