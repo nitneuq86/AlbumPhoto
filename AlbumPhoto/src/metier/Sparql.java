430e662a75ef511c1b81d37175a88d5f0281074e
Myriam <myriam.abdelfattah@gmail.com>
2016-01-14 11:00:12 +0100

    Optimisation de la classe de requêtage Sparql

--- AlbumPhoto/src/metier/Sparql.java
+++ AlbumPhoto/src/metier/Sparql.java
@@ -16,22 +16,23 @@ public class Sparql {
 	
 	private Sparql() {
 		authenticator = new SimpleAuthenticator("abdelfam", "abdelfam2015".toCharArray());
-		prefixs = "PREFIX : <http://myrquent.org/albumz#> "
+		prefixs = "PREFIX : <http://myrquent.org/albumz#>"
 				+ "PREFIX IMSS: <http://imss.upmf-grenoble.fr/abdelfam>"
 				+ "PREFIX foaf: <http://xmlns.com/foaf/0.1/> "
 				+ "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>"
 				+ "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>"
-				+ "PREFIX dc: <http://purl.org/dc/elements/1.1/>";
+				+ "PREFIX dc: <http://purl.org/dc/elements/1.1/>"
+				+ "PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>";
 	}
 	
 	public static Sparql getSparql(){
 		return SPARQL;
 	}
 	
-	public ResultSet requete(String requete){
+	public ResultSet requete(String requete, String graph){
 		try (QueryExecution qe = QueryExecutionFactory.sparqlService("https://imss-www.upmf-grenoble.fr/sparql",
 				prefixs + " " + requete, 
-				"http://imss.upmf-grenoble.fr/abdelfam",
+				graph,
 				authenticator)) {
 
 			ResultSet rs = qe.execSelect();;
