c76459f029f83dbc3165ec7ba78fa7c71fcdfa3e
Myriam <myriam.abdelfattah@gmail.com>
2016-01-14 10:57:53 +0100

    Traitement AJAX des endroits et vérification du formulaire

--- AlbumPhoto/WebContent/ressources/javascript/CRUD.js
+++ AlbumPhoto/WebContent/ressources/javascript/CRUD.js
@@ -21,9 +21,15 @@ function getXMLHttpRequest() {
 	return xhr;
 }
 
-function POST(controleur, parametres){
+function POST(controleur, parametres, callback){
 	var xhr = getXMLHttpRequest();
-
+	
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 0)) {
+			callback(xhr.responseText);
+		}
+	};
+	
 	xhr.open("POST", URL_APP + controleur, true);
 	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 	xhr.send(parametres);
@@ -45,7 +51,6 @@ function DELETE(controleur, parametres){
 	
 	xhr.onreadystatechange = function() {
 		if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 0)) {
-			console.log("bidule");
 			location.reload();
 		}
 	};
@@ -54,5 +59,96 @@ function DELETE(controleur, parametres){
 	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 	xhr.send(parametres);
 	
+	return false;
+}
+
+function submitForm(){
+	var ouString = "ou";
+	var ou = document.getElementById(ouString);
+	var liste = ou.getAttribute("list");
+	var options = document.querySelectorAll("#" + liste + " option");
+	var hiddenInput = document.getElementById(ouString + "-hidden");
+	var erreur = false;
+	var estDansLaListe = false;
+	var message = "";
+	
+	if(document.getElementById("titre").value == ""){
+		erreur = true;
+		message += "Veuillez choisir un titre. <br/>";
+	}
+	
+	if(document.getElementById("url").files.length == 0){
+		erreur = true;
+		message += "Veuillez choisir une photo. <br\>";
+	}
+
+	if(document.getElementById("date").value == ""){
+		erreur = true;
+		message += "Veuillez choisir une date. <br\>";
+	}
+
+	for(var i = 0; i < options.length; i++) {
+        var option = options[i];
+        if(option.innerText === ou.value) {
+        	estDansLaListe = true;
+            hiddenInput.value = option.getAttribute('data-value');
+            break;
+        }
+    }
+    if(!estDansLaListe){
+    	erreur = true;
+		message += "Veuillez choisir un endroit de la liste déroulante. <br/>";
+    }
+
+    document.getElementById("message").innerHTML = message;
+	
+	return !erreur;
+}
+
+function verificationPlace(){
+	var ou = document.getElementById('ou').value;
+	if(ou.length >= 3){
+		POST("Place", "place=" + ou, traitementPlaces);
+	}
+}
+
+function traitementPlaces(response){
+	response = JSON.parse(response);
+	var places = document.getElementById('places');
+	var options = "";
+	response.forEach(function(element, index, array){
+		options += '<option data-value="' + element.uri + '">' + element.place + '</option>';
+	});
+	places.innerHTML = options;
+	console.log(response);
+}
+
+function ajoutPersonne(){
+	var qui = "qui";
+	var positionNouveauQui = document.querySelectorAll("select[name='" + qui + "']").length + 1;
+	var positionCourante = positionNouveauQui <= 2 ? "" : positionNouveauQui - 1;
+	var ligneCourante = document.getElementById(qui + positionCourante).parentElement.parentElement;
+	var nouveauQui = document.createElement("tr");
+	nouveauQui.innerHTML = '<td><label >Qui :</label></td>\n'
+						 + '<td>'
+						 + '<select name="qui" id="' + qui + positionNouveauQui + '">'
+						 	+ document.getElementById(qui).innerHTML
+						 + '</select>'
+						 + '</td>';
+	ligneCourante.parentNode.insertBefore(nouveauQui, ligneCourante.nextElementSibling);
+	return false;
+}
+
+function ajoutObjet(){
+	var quoi = "quoi";
+	var positionNouveauQuoi = document.querySelectorAll("input[name='" + quoi + "']").length + 1;
+	var positionCourante = positionNouveauQuoi <= 2 ? "" : positionNouveauQuoi - 1;
+	var ligneCourante = document.getElementById(quoi + positionCourante).parentElement.parentElement;
+	var nouveauQuoi = document.createElement("tr");
+	nouveauQuoi.innerHTML = '<td><label >Quoi :</label></td>\n'
+						 + '<td>'
+						 + '<input type="text" name="quoi" id="' + quoi + positionNouveauQuoi + '"/>'
+						 + '</td>';
+	ligneCourante.parentNode.insertBefore(nouveauQuoi, ligneCourante.nextElementSibling);
 	return false;
 }
